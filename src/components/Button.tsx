fb631be9a73c2018b6da14d428f288f27018a112
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2022-10-08 20:11:13 +0530

    SharePopOver component added in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { faShareAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classes from "./Button.module.css";
+import SharePopOver from "./SharePopOver";
 
 const Button: React.FC<{
   text: string;
   bgColor: string;
   textColor: string;
 }> = ({ text, bgColor, textColor }) => {
+  const [isPopEnabled, SetIsPopEnabled] = useState(false);
+  const popOverHandler = () => {
+    SetIsPopEnabled((prevState) => !prevState);
+  };
   return (
-    <button
-      style={{
-        display: "flex",
-        backgroundColor: bgColor,
-        color: textColor,
-        padding: "8px, 16px",
-      }}
-      className={classes.button}
-    >
-      {text}
-      <FontAwesomeIcon icon={faShareAlt} />
-    </button>
+    <div>
+      <button
+        type="button"
+        style={{
+          display: "flex",
+          backgroundColor: bgColor,
+          color: textColor,
+          padding: "8px, 16px",
+        }}
+        className={classes.button}
+        onClick={popOverHandler}
+      >
+        {text}
+        <FontAwesomeIcon icon={faShareAlt} />
+      </button>
+      {isPopEnabled && <SharePopOver />}
+    </div>
   );
 };
 
